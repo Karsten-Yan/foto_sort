c5a597fcc34e164d3978606f04a6baa6611f464d
Karsten Yan <karsten.yan@gmail.com>
2021-01-22 17:59:16 +0100

    initial upload of foto sorting script

---
+++ sort.py
@@ -0,0 +1,85 @@
+import os
+import glob
+from pathlib import Path
+from datetime import datetime
+import exifread
+
+wd = os.getcwd()
+
+unsorted_folder = os.path.join(wd, "unsorted")
+
+extension = ["jpeg", "png", "mp4", "jpg", "mov", "arw", "cr2", "m4v", "avi"]
+movie = ["mp4", "mov", "m4v", "avi"]
+
+
+files = []
+for filename in glob.iglob(unsorted_folder + '**/**', recursive=True):
+    files.append(filename)
+sortfolder = os.path.join(wd, "sorted")
+if not os.path.exists(sortfolder):
+    os.makedirs(sortfolder)
+for file in files:
+    if len(file.rsplit(".", 1)) > 1 and\
+            file.rsplit(".", 1)[1].lower() in extension:
+
+        try:
+            with open(file, 'rb') as fh:
+                tags = exifread.process_file(fh, stop_tag="Image Model")
+                cmod = tags["Image Model"].printable
+        except:
+            cmod = "Generic_Model"
+        try:
+            with open(file, 'rb') as fh:
+                tags = exifread.process_file(fh, stop_tag="Image Make")
+                make = tags["Image Make"].printable
+        except:
+            make = "Generic_Make"
+
+        source = make.strip().replace(" ", "_") + "_" + cmod.strip().replace(" ", "_")
+
+        if file.rsplit(".")[1].lower() in movie:
+            source = "movie"
+        file_info = Path(file)
+
+        try:
+            with open(file, 'rb') as fh:
+                tags = exifread.process_file(fh,
+                                             stop_tag="EXIF DateTimeOriginal")
+                dateTaken = tags["EXIF DateTimeOriginal"]
+            ctime = datetime.strptime(dateTaken.printable,
+                                      '%Y:%m:%d %H:%M:%S')
+        except:
+            ctime = datetime.fromtimestamp(file_info.stat().st_mtime)
+
+        month = ctime.strftime("%m_%B")
+        year = ctime.strftime("%Y")
+        folder = os.path.join(sortfolder, year, month, source)
+
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        file_name = os.path.basename(file)
+
+        target = os.path.join(folder, file_name)
+
+        sfile = starget = os.path.getsize(file)
+        starget = 0
+        if os.path.exists(target):
+            starget = os.path.getsize(target)
+            if sfile > starget:
+                os.remove(target)
+                Path(file).rename(target)
+            else:
+                pass
+        else:
+            Path(file).rename(target)
+
+    elif len(file.rsplit(".", 1)) > 1 and len(file.rsplit(".", 1)[1]) < 5:
+        other = os.path.join(sortfolder, "other")
+
+        if not os.path.exists(other):
+            os.makedirs(other)
+
+        target = os.path.join(other, os.path.basename(file))
+        if not os.path.exists(target):
+            Path(file).rename(target)
