05b17d6147825b76d73944091e7bb74d252911f6
Karsten Yan <karsten.yan@gmail.com>
2021-01-23 16:21:00 +0100

    added statistics

--- sort.py
+++ sort.py
@@ -3,15 +3,24 @@ import glob
 from pathlib import Path
 from datetime import datetime
 import exifread
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 wd = os.getcwd()
 
 unsorted_folder = os.path.join(wd, "unsorted")
 
-extension = ["jpeg", "png", "mp4", "jpg", "mov", "arw", "cr2", "m4v", "avi"]
-movie = ["mp4", "mov", "m4v", "avi"]
+extension = ["jpeg", "png", "mp4", "jpg", "mov", "arw", "cr2", "m4v", "avi",
+             "webp", "mkv"]
+movie = ["mp4", "mov", "m4v", "avi", "mkv"]
 
+statistics_path = os.path.join(wd, "statistics.csv")
 
+if not os.path.exists(statistics_path):
+    statistics = pd.DataFrame()
+else:
+    statistics = pd.read_csv(statistics_path, index_col=0)
 files = []
 for filename in glob.iglob(unsorted_folder + '**/**', recursive=True):
     files.append(filename)
@@ -24,35 +33,30 @@ for file in files:
 
         try:
             with open(file, 'rb') as fh:
-                tags = exifread.process_file(fh, stop_tag="Image Model")
+                tags = exifread.process_file(fh)
                 cmod = tags["Image Model"].printable
-        except:
-            cmod = "Generic_Model"
-        try:
-            with open(file, 'rb') as fh:
-                tags = exifread.process_file(fh, stop_tag="Image Make")
                 make = tags["Image Make"].printable
+                dateTaken = tags["EXIF DateTimeOriginal"]
+                ctime = datetime.strptime(dateTaken.printable,
+                                          '%Y:%m:%d %H:%M:%S')
         except:
+            file_info = Path(file)
+            cmod = "Generic_Model"
             make = "Generic_Make"
+            ctime = datetime.fromtimestamp(file_info.stat().st_mtime)
 
-        source = make.strip().replace(" ", "_") + "_" + cmod.strip().replace(" ", "_")
+        source = make.strip().replace(" ", "_") + "_" + cmod.\
+            strip().replace(" ", "_")
 
         if file.rsplit(".")[1].lower() in movie:
+            ftype = "movie"
             source = "movie"
-        file_info = Path(file)
-
-        try:
-            with open(file, 'rb') as fh:
-                tags = exifread.process_file(fh,
-                                             stop_tag="EXIF DateTimeOriginal")
-                dateTaken = tags["EXIF DateTimeOriginal"]
-            ctime = datetime.strptime(dateTaken.printable,
-                                      '%Y:%m:%d %H:%M:%S')
-        except:
-            ctime = datetime.fromtimestamp(file_info.stat().st_mtime)
+        else:
+            ftype = "foto"
 
         month = ctime.strftime("%m_%B")
         year = ctime.strftime("%Y")
+        day = ctime.strftime("%d")
         folder = os.path.join(sortfolder, year, month, source)
 
         if not os.path.exists(folder):
@@ -74,6 +78,12 @@ for file in files:
         else:
             Path(file).rename(target)
 
+        temp_stat = pd.DataFrame([[ftype, make, cmod,
+                                   ctime, year, month, day]],
+                                 index=[file_name])
+        temp_stat["date"] = temp_stat[3].dt.to_period("D")
+        statistics = pd.concat([statistics, temp_stat], axis=0)
+
     elif len(file.rsplit(".", 1)) > 1 and len(file.rsplit(".", 1)[1]) < 5:
         other = os.path.join(sortfolder, "other")
 
@@ -83,3 +93,18 @@ for file in files:
         target = os.path.join(other, os.path.basename(file))
         if not os.path.exists(target):
             Path(file).rename(target)
+
+statistics.to_csv(statistics_path)
+plot_stats = statistics.copy()
+plot_stats.columns = ["file_type", "camera_maker", "camera_model",
+                      "timestamp", "year", "month", "day", "date"]
+by_day = plot_stats[plot_stats.camera_maker != "Generic_Make"].\
+    groupby(["camera_maker", "date"]).count().iloc[:, 0]
+by_day =  by_day.reset_index()
+by_day = by_day.pivot("date","camera_maker","file_type").to_timestamp()
+
+fig, ax = plt.subplots(1,1)
+sns.lineplot(data = by_day, ax=ax)
+for label in ax.get_xticklabels():
+    label.set_ha("right")
+    label.set_rotation(45)
